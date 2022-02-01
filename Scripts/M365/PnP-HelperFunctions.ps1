7bdd6c8566982f28b1c47f8e493509e2475e6c21
Sjoerd de Valk <sjoerddevalk@outlook.com>
2022-02-01 23:02:35 +0100

    Updated all executable script files with GPLv3 statements

--- Scripts/M365/PnP-HelperFunctions.ps1
+++ Scripts/M365/PnP-HelperFunctions.ps1
@@ -1,7 +1,20 @@
 <#
 .SYNOPSIS
 
-By: Sjoerd de Valk, SP de Valk Consultancy, 2020
+Configuration as Code - Azure DevOps Scafold and Pipeline
+Copyright (C) 2021  Sjoerd de Valk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 All helper functions for the SharePoint PnP module are loaded here, to be used by a main function
 .DESCRIPTION
 
